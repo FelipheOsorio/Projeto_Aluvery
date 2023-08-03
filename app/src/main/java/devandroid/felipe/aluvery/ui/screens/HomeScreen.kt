fd3a36723083f70a289c63c23d1461615d37201d
FelipheOsorio <feeh_lepinhoo@hotmail.com>
2023-08-02 23:39:26 -0300

    refatorando com lazy layouts

--- app/src/main/java/devandroid/felipe/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/devandroid/felipe/aluvery/ui/screens/HomeScreen.kt
@@ -1,11 +1,10 @@
 package devandroid.felipe.aluvery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,22 +17,18 @@ import devandroid.felipe.aluvery.ui.theme.AluveryTheme
 
 @Composable
 fun HomeScreen(sections: Map<String, List<ProductModel>>) {
-    Column(
+    LazyColumn(
         Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(vertical = 16.dp)
     ) {
-        Spacer(Modifier)
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            ProductSection(
-                title = title,
-                products = products
-            )
+        itemsIndexed(sections.toList()) { _, section ->
+            val title = section.first
+            val product = section.second
+
+            ProductSection(title = title, products = product)
         }
-        Spacer(Modifier)
     }
 }
 
